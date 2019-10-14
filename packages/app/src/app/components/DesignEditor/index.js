2cbc9041637706756bb235c195e4449d121d8597
DutchDestroyer <wijnbergenmark@gmail.com>
2019-10-14 23:56:27 +0200

    starting with the design editor2

--- packages/app/src/app/components/DesignEditor/index.js
+++ packages/app/src/app/components/DesignEditor/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Dropdown from 'react-dropdown';
+
+const fontStyles = [
+  { value: 'Times New Roman', label: 'Times New Roman' },
+  { value: 'Arial', label: 'Arial' },
+];
 
 class DesignEditor extends React.PureComponent {
   render() {
-    return <div>ABCDEF</div>;
+    return (
+      <>
+        <label htmlFor="fontSize">
+          Name:
+          <input id="fontSize" type="text" placeholder="def" />
+        </label>
+        <div>
+          <Dropdown options={fontStyles} placeholder="Select a fontfamily" />
+        </div>
+      </>
+    );
   }
 }
 
